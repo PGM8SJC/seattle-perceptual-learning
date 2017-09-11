c06c7dbb471ca04686d52bfd313aa0e7c077641a
Asier Erramuzpe <asier.erramuzpe@gmail.com>
2017-09-11 15:11:05 -0700

    modified fashion_mnist to match data

--- perclearn/fashion_mnist_cnn.py
+++ perclearn/fashion_mnist_cnn.py
@@ -7,7 +7,7 @@ Gets to 99.25% test accuracy after 12 epochs
 
 from __future__ import print_function
 import keras
-from keras.datasets import mnist
+from keras.datasets import fashion_mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
@@ -21,7 +21,7 @@ epochs = 12
 img_rows, img_cols = 28, 28
 
 # the data, shuffled and split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
 if K.image_data_format() == 'channels_first':
     x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
