6d55b2708356083bf72b4b677e50429f0b628cd2
Asier Erramuzpe <asier.erramuzpe@gmail.com>
2017-09-13 16:04:59 -0700

    created experiment data

--- perclearn/test_spyder.py
+++ perclearn/test_spyder.py
@@ -25,21 +25,6 @@ print(X_train.shape)
 plt.imshow(np.reshape(X_train[0,:], (28,28)), cmap=plt.cm.Greys)
 print(y_train[0])
 
-a = np.reshape(X_train[0,:], (28,28))
-b = X_train[0,:]
-
-# Flattening function
-np.ndarray.flatten()
-
-plt.imshow(create_2D_noise())
-
-
-a = np.reshape(X_train[0,:], (28,28))
-b = scale_2D(create_2D_noise())
-
-result = create_composition(a, b,
-                       x_offset=0, y_offset=0,
-                       center=None, radius=None)
 
 
 plt.imshow(result)
@@ -53,26 +38,25 @@ def create_new_dataset(dataset, offsets=[[0,0]]):
     new_dataset = np.zeros((m,n*4))
     
     for i in range(m):
-        image = np.reshape(X_train[i,:], (im_x,im_x))
+        image = np.reshape(dataset[i,:], (im_x,im_x))
         noise_bg = scale_2D(create_2D_noise())
         
         rand_offset = np.random.randint(0,num_offsets)
 
         result = create_composition(image, noise_bg,
-                       x_offset=offsets[rand_offset,0],
-                       y_offset=offsets[rand_offset,1],
+                       x_offset=offsets[rand_offset][0],
+                       y_offset=offsets[rand_offset][1],
                        center=None, radius=None)
         
         new_dataset[i,:] = np.ndarray.flatten(result)
         
     return new_dataset
     
+X_test = create_new_dataset(X_test, [[0,28],[28,0],[28,28]])    
+    
+
     
     
-    
-    
-    
-    
-    
+np.savez(opj(cwd, 'perclearn/data/experiments/1/testing'),X_test)
     
     
