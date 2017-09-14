e44dc20b423302e9a94a07b57fa967bef121d693
Asier Erramuzpe <asier.erramuzpe@gmail.com>
2017-09-14 10:50:31 -0700

    correction2

--- perclearn/fashion_mnist_cnn_experiment1.py
+++ perclearn/fashion_mnist_cnn_experiment1.py
@@ -9,7 +9,6 @@ Gets to % test accuracy after 12 epochs
 
 from __future__ import print_function
 import keras
-from keras.datasets import fashion_mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
