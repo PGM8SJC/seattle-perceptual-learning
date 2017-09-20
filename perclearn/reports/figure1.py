9fcb2ea4330261e47b028a459de0862fbba25f86
Asier Erramuzpe <asier.erramuzpe@gmail.com>
2017-09-20 10:16:29 -0700

    figure 1

--- perclearn/reports/figure1.py
+++ perclearn/reports/figure1.py
@@ -4,3 +4,28 @@ from os.path import join as opj
 import numpy as np
 import matplotlib.pyplot as plt
 
+cwd = os.getcwd()
+
+# results[0] = test loss; results[1] = test accuracy
+translate_results = np.load(opj(cwd, 'perclearn/data/results/translate.npz'))['arr_0']
+rotate_results = np.load(opj(cwd, 'perclearn/data/results/rotate.npz'))['arr_0']
+
+# Translation plots
+x = np.linspace(0, 28, 28)
+
+f, axarr = plt.subplots(2, sharex=True)
+axarr[0].scatter(x, translate_results[0,:])
+axarr[0].set_title('Test Loss - Translation')
+axarr[1].scatter(x, translate_results[1,:])
+axarr[1].set_title('Test Accuracy - Translation')
+plt.savefig('/home/asier/Desktop/translate.png')
+# Rotation plots
+plt.figure()
+x = np.linspace(0, 360, 36)
+
+f, axarr = plt.subplots(2, sharex=True)
+axarr[0].scatter(x, rotate_results[0,:])
+axarr[0].set_title('Test Loss - Rotation')
+axarr[1].scatter(x, rotate_results[1,:])
+axarr[1].set_title('Test Accuracy - Rotation')
+plt.savefig('/home/asier/Desktop/rotate.png')
