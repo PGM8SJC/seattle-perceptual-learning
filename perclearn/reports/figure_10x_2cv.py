7bd86b56cd252129acf4c78502f71ca504f738b2
Asier Erramuzpe <asier.erramuzpe@gmail.com>
2017-09-21 10:34:38 -0700

    finished plot2, waiting for results

--- perclearn/reports/figure_10x_2cv.py
+++ perclearn/reports/figure_10x_2cv.py
@@ -8,34 +8,53 @@ import seaborn as sns
 cwd = os.getcwd()
 
 # results[0] = test loss; results[1] = test accuracy
-translate_results = np.load(opj(cwd, 'perclearn/data/results/translate_10x_2cv.npz.npz'))['arr_0']
-rotate_results = np.load(opj(cwd, 'perclearn/data/results/rotate_10x_2cv.npz'))['arr_0']
+# 2CV
+translate_10x_2cv_rand = np.load(opj(cwd, 'perclearn/data/results/translate_10x_2cv.npz'))['arr_0']
+rotate_10x_2cv_rand = np.load(opj(cwd, 'perclearn/data/results/rotate_10x_2cv.npz'))['arr_0']
 
-# Translation plots
-gammas = sns.load_dataset("gammas")
-ax = sns.tsplot(time="timepoint", value="BOLD signal",
-                unit="subject", condition="ROI",
-                data=gammas)
+translate_2cv_fixed = np.load(opj(cwd, 'perclearn/data/results/translate.npz'))['arr_0']
+rotate_2cv_fixed= np.load(opj(cwd, 'perclearn/data/results/rotate.npz'))['arr_0']
+
+# VGG16
+translate_10x_vgg16_rand = np.load(opj(cwd, 'perclearn/data/results/translate_10x_vgg16.npz'))['arr_0']
+rotate_10x_vgg16_rand = np.load(opj(cwd, 'perclearn/data/results/rotate_10x_vgg16.npz'))['arr_0']
+
+translate_vgg16_fixed = np.load(opj(cwd, 'perclearn/data/results/translate_exp2_vgg16.npz'))['arr_0']
+rotate_vgg16_fixed= np.load(opj(cwd, 'perclearn/data/results/rotate_exp2_vgg16.npz'))['arr_0']
 
 
+"""
+plots
+"""
+# TRANSLATE
+sns.tsplot(translate_10x_2cv_rand[:,1,:])
+plt.plot(translate_2cv_fixed[1,:])
+sns.tsplot(translate_10x_vgg16_rand[:,1,:])
+plt.plot(translate_vgg16_fixed[1,:])
+plt.legend(['Fixed 2cv', 'Random 10x_2cv',
+            'Fixed vgg16', 'Random 10x_vgg16'])
+plt.title('TRANSLATION')
+plt.xlabel('Offset x axis')
+plt.ylabel('Accuracy')
 
-
-x = np.linspace(0, 28, 28)
-
-f, axarr = plt.subplots(2, sharex=True)
-axarr[0].scatter(x, translate_results[0,:])
-axarr[0].set_title('Test Loss - Translation')
-axarr[1].scatter(x, translate_results[1,:])
-axarr[1].set_title('Test Accuracy - Translation')
-plt.savefig('/home/asier/Desktop/translate_exp3.png')
+plt.savefig('/home/asier/Desktop/translate_2cv_vs_vgg16.png')
 # Rotation plots
+
+
+
+#ROTATE
 plt.figure()
 x = np.linspace(0, 360, 36)
 
-f, axarr = plt.subplots(2, sharex=True)
-axarr[0].scatter(x, rotate_results[0,:])
-axarr[0].set_title('Test Loss - Rotation')
-axarr[1].scatter(x, rotate_results[1,:])
-axarr[1].set_title('Test Accuracy - Rotation')
-plt.savefig('/home/asier/Desktop/rotate_exp3.png')
+sns.tsplot(rotate_10x_2cv_rand[:,1,:], time=x)
+plt.plot(x, rotate_2cv_fixed[1,:])
+sns.tsplot(translate_10x_vgg16_rand[:,1,:], time=x)
+plt.plot(translate_vgg16_fixed[1,:])
+plt.legend(['Fixed 2cv', 'Random 10x_2cv',
+            'Fixed vgg16', 'Random 10x_vgg16'])
+plt.title('ROTATION')
+plt.xlabel('Angle rot')
+plt.ylabel('Accuracy')
+
+plt.savefig('/home/asier/Desktop/rotate_2cv_vs_vgg16.png')
 
